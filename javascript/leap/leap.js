96176308cbdb9184778f6964f892b4b0f55b44f4
Jean Matheus Souto <jeanmatheussouto@gmail.com>
2017-01-28 17:07:35 -0200

    resolve leap specs

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -1,14 +1,15 @@
-//
-// This is only a SKELETON file for the "Bob" exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+var Year = function(year) {
+  this.year = year;
+};
 
-var Year = function() {};
-
-Year.prototype.isLeap = function(input) {
-//
-// YOUR CODE GOES HERE
-//
+Year.prototype.isLeap = function() {
+  if (this.year % 100 == 0) {
+    if (this.year % 400 == 0) {
+      return true;
+    }
+    return false;
+  }
+  return this.year % 4 == 0;
 };
 
 module.exports = Year;
